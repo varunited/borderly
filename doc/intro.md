1968dae3c274724ad45ae2f8f55713e47de32775
varunited <varunited@gmail.com>
2019-07-27 19:07:25 +0530

    update project.clj and add cheshire
    - remove doc directory

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to borderly
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject borderly "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Utility library for Ring based web applications"
+  :url "https://github.com/varunited/borderly"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [cheshire  "5.8.1"]]
   :repl-options {:init-ns borderly.core})
